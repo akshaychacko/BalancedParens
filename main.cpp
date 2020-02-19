52dec3b4007928bd98dc3bd3dbe03cb814a2136e
Akshay Chacko <akshaychacko999@gmail.com>
2020-02-19 16:58:57 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,56 @@
+// Project Identifier: 1CAEF3A0FEDD0DEC26BA9808C69D4D22A9962768
+
+#include <deque>
+#include <iostream>
+using namespace std;
+
+
+bool isBalanced() {
+	deque<char> structure;
+	char input = ' ';
+	while (cin >> input) {
+		if (input != ' ') {
+			if (input == '[' || input == '(' || input == '{') {
+				structure.push_front(input);
+			}
+
+			///	else if ((input == ']' && structure.front() == '[') || (input == ')' && structure.front() == '(') || (input == '}' && structure.front() == '}')) {
+			else if ((input == ']') || (input == ')') || (input == '}')) {
+				char curr = structure.front();
+				structure.pop_front();
+				switch (input) {
+				case ')' :
+					if (curr != '(') {
+						return false;
+					}
+				break;
+				case '}' :
+					if (curr != '{') {
+						return false;
+					}
+				break;
+				case ']':
+					if (curr != '[') {
+						return false;
+					}
+				break;
+				}		
+			}
+		}
+	}
+
+	if (!structure.empty()) {
+		return false;
+	}
+	return true;
+}
+
+int main() {
+
+	if (isBalanced()) {
+		cout << "Balanced";
+	}
+	else {
+		cout << "Not balanced";
+	}
+}
